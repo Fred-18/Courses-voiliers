5c289ca24dd7e06dc78ecc2bd59c57312874c6ea
Fred Nobre <frednobre18@gmail.com>
2022-09-05 11:57:24 +0200

    first commit

---
+++ src/test/java/com/CourseVoiliers/FlotsBleus/FlotsBleusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CourseVoiliers.FlotsBleus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlotsBleusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
